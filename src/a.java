c6e13518d7881a06b47592ca8d43cc5ce9b6b310
Yongqinchuan Du <duqcyxwd@gmail.com>
2012-10-19 14:46:06 -0400

    add start

--- src/a.java
+++ src/a.java
@@ -1,12 +1,11 @@
 
-public class adsfasdf {
+public class a {
 
     /**
      * @param args
      */
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        System.out.println("Hello");
 
     }
 
