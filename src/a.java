d257b00dcd509221ab2e397e235f512fba0ba87b
Yongqinchuan Du <duqcyxwd@gmail.com>
2012-10-20 14:46:34 -0400

    clean

--- src/a.java
+++ src/a.java
@@ -5,18 +5,7 @@ public class a {
      */
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-
-        
-        //want a clean a
-        
-        //master
-        //clean is good
-
-// ok you are doing goo
-
-//??
-//????
-// ok you are doing realy good
+        //local clean
     }
 
 }
